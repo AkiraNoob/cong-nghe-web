6bd240558a99e604c1380084882af81043d3dc9e
trungtinhn <trungtinh1620@gmail.com>
2023-12-12 22:13:27 +0700

    [FE] update HomePage

--- frontend/src/app/home/page.tsx
+++ frontend/src/app/home/page.tsx
@@ -1,11 +1,87 @@
 "use client";
-import React, { useState } from 'react';
+import React from 'react';
+import CourseComponent from '~/components/course_component';
 import HeaderComponent from '~/components/header_component';
 const HomePage: React.FC = () => {
-  const [isMobile, setIsMobile] = useState(false);
+  // Data simple
+  const data = [
+    {
+      id: '1',
+      image: '/images/java.jpg',
+      name: 'Lập trình C/C++',
+      date: '12/12/2023',
+      rating: 4.0,
+      views: 4
+    },
+    {
+      id: '2',
+      image: '/images/cplusplus.jpg',
+      name: 'Lập trình Java trên ứng dụng di động',
+      date: '12/12/2023',
+      rating: 3.6,
+      views: 4
+    },
+    {
+      id: '3',
+      image: '/images/java.jpg',
+      name: 'Lập trình C/C++',
+      date: '12/12/2023',
+      rating: 4.0,
+      views: 4
+    },
+    {
+      id: '4',
+      image: '/images/cplusplus.jpg',
+      name: 'Lập trình Java trên ứng dụng di động',
+      date: '12/12/2023',
+      rating: 3.6,
+      views: 4
+    },
+    {
+      id: '5',
+      image: '/images/java.jpg',
+      name: 'Lập trình C/C++',
+      date: '12/12/2023',
+      rating: 4.0,
+      views: 4
+    },
+    {
+      id: '6',
+      image: '/images/cplusplus.jpg',
+      name: 'Lập trình Java trên ứng dụng di động',
+      date: '12/12/2023',
+      rating: 3.6,
+      views: 4
+    },
+    // Add more data items as needed
+  ];
+  const handleClick = (param: String): void =>{
+
+  }
   return (
     <div>
-        <HeaderComponent></HeaderComponent>
+        <div>
+          <HeaderComponent></HeaderComponent>
+        </div>
+        <div className='p-10'>
+          <h3 className='text-2xl font-bold'>Danh sách khóa học</h3>
+          <div className="mt-8 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10">
+          {data.map((item, index) => (
+          <div key={index}>
+            <CourseComponent
+              id={item.id}
+              name={item.name}
+              date={item.date}
+              image={item.image}
+              rating={item.rating}
+              views={item.views}
+              onClick={ () => handleClick(item.id)}
+            >
+            </CourseComponent>
+            </div>
+          ))}
+        </div>
+        </div>
     </div>
   );
 };
--- frontend/src/components/header_component.tsx
+++ frontend/src/components/header_component.tsx
@@ -1,5 +1,6 @@
 "use client";
-import { Logout, Settings } from '@mui/icons-material';
+import { Login, Logout, PersonAdd, Settings } from '@mui/icons-material';
+import MenuIcon from '@mui/icons-material/Menu';
 import { ListItemIcon, Menu, MenuItem, Typography } from '@mui/material';
 import Avatar from '@mui/material/Avatar';
 import Box from '@mui/material/Box';
@@ -7,10 +8,11 @@ import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import Tooltip from '@mui/material/Tooltip';
 import Image from 'next/image';
+import Link from 'next/link';
 import React from 'react';
 import SearchComponent from './search_component';
 const HeaderComponent: React.FC = () => {
-  const [auth, setAuth] = React.useState(true);
+  const [auth, setAuth] = React.useState(false);
 
   const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
   const open = Boolean(anchorEl);
@@ -26,14 +28,15 @@ const HeaderComponent: React.FC = () => {
   };
   return (
     <div className='flex items-center bg-white border-b-2 border-gray-200 sticky top-0 z-20 h-20'>
-      <div className='items-center w-1/3 flex ml-10'>
+      <div className='items-center w-1/6 flex sm:ml-10 ml-3 sm:w-1/3'>
+        {/* <img src='/images/CodePro.png' className='rounded-sm h-10 w-10'>
+        </img> */}
         <Image
-          className='rounded-sm'
-          src="/images/CodePro.png"
-          alt="Mô tả hình ảnh"
-          width={50}
-          height={50}
-        />
+          src={"/images/CodePro.png"}
+          width={40}
+          height={40}
+          alt='Logo'
+        ></Image>
         <Typography
             variant="h6"
             noWrap
@@ -52,10 +55,10 @@ const HeaderComponent: React.FC = () => {
             CODEPRO
         </Typography>
       </div>
-      <div className='w-1/3 hidden md:block'>
-        <SearchComponent onSearch={handleSearch}></SearchComponent>
+      <div className='w-4/6 sm:w-1/3'>
+        <SearchComponent></SearchComponent>
       </div>
-      <div className='w-1/3 hidden md:block mr-10'>
+      <div className='w-1/6 sm:mr-10 mr-0.5 sm:w-1/3'>
         {auth ? (
           <Box sx={{ display: 'flex', justifyContent: 'flex-end'}}>
             <Tooltip title="Open profile">
@@ -116,10 +119,96 @@ const HeaderComponent: React.FC = () => {
             </Menu>
           </Box>
         ):(
-        <Box sx={{ display: 'flex', justifyContent: 'flex-end'}}>
-            <Button color="secondary" variant="outlined" sx={{ textTransform: 'none', marginRight: 3}}>Đăng nhập</Button>
-            <Button color="inherit" variant= "outlined" sx={{ textTransform: 'none'}}>Đăng ký</Button>
-        </Box>
+        <div>
+           <div className='hidden sm:block'>
+              <Box sx={{ display: 'flex', justifyContent: 'flex-end'}}>
+                <Link
+                      href="/register"
+                      className="font-medium hover:underline "
+                    >
+                     <Button color="secondary" variant="outlined" sx={{ textTransform: 'none', marginRight: 3}}>Đăng nhập</Button>
+                </Link>
+                <Link
+                      href="/register"
+                      className="font-medium hover:underline "
+                    >
+                    <Button color="inherit" variant= "outlined" sx={{ textTransform: 'none'}}>Đăng ký</Button>
+                </Link>
+              </Box>
+            </div>
+            <div className='block md:hidden'>
+              <Box sx={{ display: 'flex', justifyContent: 'flex-end'}}>
+                <IconButton
+                  size="large"
+                  edge="start"
+                  color="inherit"
+                  aria-label="menu"
+                  onClick={handleClick}
+                >
+                  <MenuIcon />
+                </IconButton>
+                <Menu
+                  anchorEl={anchorEl}
+                  id="account-menu"
+                  open={open}
+                  onClose={handleClose}
+                  onClick={handleClose}
+                  PaperProps={{
+                    elevation: 0,
+                    sx: {
+                      overflow: 'visible',
+                      filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
+                      mt: 1.5,
+                      '& .MuiAvatar-root': {
+                        width: 32,
+                        height: 32,
+                        ml: -0.5,
+                        mr: 1,
+                      },
+                      '&:before': {
+                        content: '""',
+                        display: 'block',
+                        position: 'absolute',
+                        top: 0,
+                        right: 14,
+                        width: 10,
+                        height: 10,
+                        bgcolor: 'background.paper',
+                        transform: 'translateY(-50%) rotate(45deg)',
+                        zIndex: 0,
+                      },
+                    },
+                  }}
+                  transformOrigin={{ horizontal: 'right', vertical: 'top' }}
+                  anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
+                >
+                   <Link
+                      href="/register"
+                      className="font-medium hover:underline "
+                    >
+                      <MenuItem onClick={handleClose}>
+                        <ListItemIcon>
+                          <PersonAdd fontSize="small" />
+                        </ListItemIcon>
+                        Đăng ký
+                      </MenuItem>
+                    </Link>
+                    <Link
+                      href="/login"
+                      className="font-medium hover:underline "
+                    >
+                     <MenuItem onClick={handleClose}>
+                        <ListItemIcon>
+                          <Login fontSize="small" />
+                        </ListItemIcon>
+                        Đăng nhập
+                      </MenuItem>
+                    </Link>
+                </Menu>
+              </Box>
+            </div>
+        </div>
+       
         )}
       </div>
     </div>
