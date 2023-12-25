7e8a5edc40ebbc76f94ba3462f2833be894d1cbd
hauyuki <21520835@gm.uit.edu.vn>
2023-12-26 00:08:19 +0700

    [FE] Config Lesson Page

--- frontend/src/app/lesson/page.tsx
+++ frontend/src/app/lesson/page.tsx
@@ -1,19 +1,27 @@
 'use client';
+import 'next-cloudinary/dist/cld-video-player.css';
 import React from 'react';
 import HeaderComponent from '~/components/header_component';
-import YouTubePlayer from '~/components/videoplayer_component';
-
-const LessonPage: React.FC = (videoId) => {
+import PaginationComponent from '~/components/pagination_component';
+import VideoPlayer from '~/components/videoplayer_component';
+const LessonPage: React.FC = () => {
   // Data simple
-
+  const videoPublicId = 'lesson1';
   // Add more data items as needed
-  const handleClick = (param: String): void => {};
+
   return (
     <div>
       <div>
         <HeaderComponent></HeaderComponent>
       </div>
-      <YouTubePlayer videoId="-jV06pqjUUc" />
+      <div className="container mx-auto max-w-full bg-black">
+        <div className="max-w-4xl mx-auto">
+          <VideoPlayer publicId={videoPublicId} />
+        </div>
+      </div>
+      <div>
+        <PaginationComponent></PaginationComponent>
+      </div>
     </div>
   );
 };
