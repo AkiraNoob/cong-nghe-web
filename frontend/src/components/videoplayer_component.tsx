8b730d11a90c5718e657d13070c52e1ad1044b03
AkiraNoob <narutoducphuong76@gmail.com>
2023-12-27 01:35:01 +0700

    remove package need in source dir

--- frontend/src/components/videoplayer_component.tsx
+++ frontend/src/components/videoplayer_component.tsx
@@ -1,30 +1,3 @@
-// import React from 'react';
-// import YouTube from 'react-youtube';
-
-// const YouTubePlayer: React.FC = ({ videoId }) => {
-//   // Set up event handlers
-//   const onReady = (event: any) => {
-//     // Access the player instance
-//     const player = event.target;
-
-//     // For example, you can automatically play the video
-//     player.playVideo();
-//   };
-
-//   const onError = (error: any) => {
-//     console.error('YouTube Player Error:', error);
-//   };
-
-//   return (
-//     <div>
-//       <YouTube>
-//         videoId={videoId} onReady={onReady} onError={onError}{' '}
-//       </YouTube>
-//     </div>
-//   );
-// };
-
-// export default YouTubePlayer;
 'use client';
 import { CldVideoPlayer } from 'next-cloudinary';
 import React from 'react';
