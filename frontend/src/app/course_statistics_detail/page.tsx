7e69d16d29bac95213b7641ac82d050fcf50cd54
trungtinhn <trungtinh1620@gmail.com>
2023-12-29 05:01:33 +0700

    [FE] Add View Course Statistics

---
+++ frontend/src/app/course_statistics_detail/page.tsx
@@ -0,0 +1,11 @@
+'use client';
+import HeaderComponent from '~/components/header_component';
+
+export const CourseStatisticsDetail = () => {
+  return (
+    <div>
+      <HeaderComponent></HeaderComponent>
+      Chi tiet khoa hoc ne
+    </div>
+  )
+}
